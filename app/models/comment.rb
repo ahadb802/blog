bb49da5e95f2205bb432f4f311000ee666903aec
ahadb802 <abdulahadb802@gmail.com>
2023-04-08 13:27:39 +0500

    update app file

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, counter_cache: true
 
+  validates :text, presence: true
   # def self.update_post_comments_counter(comment)
   #   post = comment.post
   #   post.update(comments_counter: post.comments.counter)
